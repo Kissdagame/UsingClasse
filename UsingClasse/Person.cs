587f58d4a13b5e6cfb29b0b0208ea5549d556966
Jamar Johnson <38444511+Kissdagame@users.noreply.github.com>
2022-09-22 11:03:01 -0700

    Add project files.

---
+++ UsingClasse/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingClasse
+{
+	internal class Person
+	{
+		public string Name;
+		public int Cash;
+
+		/// <summary>
+		/// Writes Name and amount to console
+		/// </summary>
+		public void WriteMyInfo()
+		{
+			Console.WriteLine(Name + " has " + Cash + " bucks ");
+		}
+
+		/// <summary>
+		/// Give cash and removed from wallet (or prints to the console if there is not enough cash)
+		/// </summary>
+		/// <param name="amount">Amount of Cash to give</param>
+		/// <returns>The amount of cash removed from the wallet ( if the amount is invalid)</returns>
+		public int GiveCash(int amount)
+		{
+			if (amount <= 0)
+			{
+				Console.WriteLine(Name + " say: " + amount + " is not a valid amount");
+				return 0;
+			}
+			if (amount > Cash)
+			{
+				Console.WriteLine(Name + " say: " + " I do not have that amount to give " + amount);
+				return 0;
+			}
+			Cash -= amount;
+			return amount;
+		}
+
+		/// <summary>
+		/// Recieving cash and adding it to the wallet ( or printing to the console the amount is invalid)
+		/// </summary>
+		/// <param name="amount">Amount of cash given.	</param>
+		public void ReciveCash(int amount)
+		{
+			if (amount <= 0)
+			{
+				Console.WriteLine(Name + " say: " + amount + " is not an amount I will take ");
+			}
+			else
+			{
+				Cash += amount;
+			}
+		}
+	}
+}
---
+++ UsingClasse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingClasse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
